3e2a82f0943456858300b6535d78118bab4f3416
root <root@DESKTOP-34ISGJL.localdomain>
2021-10-21 21:48:38 -0400

    first commit

---
+++ bill_sharing/bill_share/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+bills = [
+    {
+        'author': 'EricF',
+        'description': 'Name of some item',
+        'cost': 'some currency value',
+        'date_posted': 'October 20, 2021'
+    },
+        {
+        'author': 'AnotherUserA',
+        'description': 'Name of some other item',
+        'cost': 'a currency value',
+        'date_posted': 'October 21, 2021'
+    }
+]
+### Video Tutorial
+## https://www.youtube.com/watch?v=qDwdMDQ8oX4
+###
+def home(request):
+    context = {
+        'bills': bills
+    }
+    return render(request, 'bill_share/home.html', context)
+    #return HttpResponse('<h1> Billing Home </h1>')
+
+def about(request):
+    return render(request, 'bill_share/about.html',{'title' : 'About'})
+    #return HttpResponse('<h1> Billing About</h1>')
+
+    
