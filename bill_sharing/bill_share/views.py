523e7712e4df5baf62932d74efba121cc4822c22
Fikeem <user@DESKTOP-AQRO905.localdomain>
2021-10-31 21:30:38 -0400

    LoginRequiredMixin added to all 'bill' pages

--- bill_sharing/bill_share/views.py
+++ bill_sharing/bill_share/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.views.generic import (
     ListView,
@@ -19,18 +20,22 @@ def home(request):
     return render(request, 'bill_share/home.html', context)
     #return HttpResponse('<h1> Billing Home </h1>')
 
-class BillListView(ListView):
+class BillListView(LoginRequiredMixin, ListView):
     model = Bill
     template_name = 'bill_share/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'bills'
     ordering = ['-date_posted']
 
-class BillDetailView(DetailView):
+class BillDetailView(LoginRequiredMixin, DetailView):
     model = Bill
     
-class BillCreateView(CreateView):
+class BillCreateView(LoginRequiredMixin, CreateView):
     model = Bill
     fields = ['title', 'content', 'cost']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'bill_share/about.html',{'title' : 'About'})
