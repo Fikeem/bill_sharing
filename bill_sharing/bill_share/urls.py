5397f27fd8763d9285d8b501acc69ba26515aee6
Fikeem <user@DESKTOP-AQRO905.localdomain>
2021-11-01 19:02:45 -0400

    commit before merge

--- bill_sharing/bill_share/urls.py
+++ bill_sharing/bill_share/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+from .views import (
+    BillListView, 
+    BillDetailView, 
+    BillCreateView, 
+    BillUpdateView,
+    BillDeleteView
+    )
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='billing-home'),
+    path('', BillListView.as_view(), name='billing-home'),
+    path('bill/<int:pk>/', BillDetailView.as_view(), name='bill-detail'),
+    path('bill/new/', BillCreateView.as_view(), name='bill-create'),
+    path('bill/<int:pk>/update', BillUpdateView.as_view(), name='bill-update'),
+    path('bill/<int:pk>/delete', BillDeleteView.as_view(), name='bill-delete'),
     path('about/', views.about, name='billing-about'),
+
 ]
+
--- bill_sharing/bill_share/views.py
+++ bill_sharing/bill_share/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
 from .models import Bill
 
 # Create your views here.
 
-bills = [
-    {
-        'author': 'EricF',
-        'description': 'Grocery Bill',
-        'cost': '$ 212.35',
-        'date_posted': 'October 20, 2021'
-    },
-        {
-        'author': 'AnotherUserA',
-        'description': 'Name of some other item',
-        'cost': 'a currency value',
-        'date_posted': 'October 21, 2021'
-    }
-]
 ### Video Tutorial
 ## https://www.youtube.com/watch?v=qDwdMDQ8oX4
 ###
@@ -27,6 +21,41 @@ def home(request):
     }
     return render(request, 'bill_share/home.html', context)
     #return HttpResponse('<h1> Billing Home </h1>')
+
+class BillListView(LoginRequiredMixin, ListView):
+    model = Bill
+    template_name = 'bill_share/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'bills'
+    ordering = ['-date_posted']
+
+class BillDetailView(LoginRequiredMixin, DetailView):
+    model = Bill
+    
+class BillCreateView(LoginRequiredMixin, CreateView):
+    model = Bill
+    fields = ['title', 'content', 'cost']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BillUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Bill
+    fields = ['title', 'content', 'cost']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        bill = self.get_object()
+        if self.request.user == bill.author:
+            return True
+        return False
+
+class BillDeleteView(LoginRequiredMixin, DeleteView):
+    model = Bill
+    success_url = '/'
 
 def about(request):
     return render(request, 'bill_share/about.html',{'title' : 'About'})
