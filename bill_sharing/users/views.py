613df56a88e0de6907c62e1038420ef0136c60c6
emmuhh <emma.steuer1@gmail.com>
2021-11-08 18:25:13 -0500

    fixed update user
    fixed update user and things associated with that video 9

--- bill_sharing/users/views.py
+++ bill_sharing/users/views.py
@@ -6,21 +6,32 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
   
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,
+                                    instance=request.user.profile)
+                                
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account updated!')
+            return redirect('profile')
 
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form' : u_form,
@@ -28,4 +39,4 @@ def profile(request):
 
     }
 
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', context)
