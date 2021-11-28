f227161ee159ea1c3e586a91e3eb8ef7dc0b2348
E Fike <fikeem@g.cofc.edu>
2021-11-27 20:34:10 -0500

    added group app; Group model is WIP; updated Bill model

--- bill_sharing/bill_share/models.py
+++ bill_sharing/bill_share/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import related
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from groups.models import Group
 
 class Bill(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    cost = models.PositiveIntegerField()
-    
+    group = models.ForeignKey(Group,on_delete=models.CASCADE)
+    cost = models.DecimalField(...,max_digits=7, decimal_places=2)
 
     def __str__(self):
         return self.title
