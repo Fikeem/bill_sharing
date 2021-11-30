2161253c0d424772cea25ca128d94110c76af1f9
E Fike <fikeem@g.cofc.edu>
2021-11-30 14:05:57 -0500

    project restructure & seperation of unfinished groups logic

--- bill_sharing/bill_sharing/urls.py
+++ bill_sharing/bill_sharing/urls.py
@@ -22,12 +22,12 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bill_share/', include('bill_share.urls')),
+    path('bills/', include('bills.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', include('bill_share.urls')),
+    path('', include('bills.urls')),
 ]
 
 
---
+++ bill_sharing/bills/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
+from .models import Bill
+
+# Create your views here.
+
+### Video Tutorial
+## https://www.youtube.com/watch?v=qDwdMDQ8oX4
+###
+def home(request):
+    context = {
+        'bills': Bill.objects.all()
+    }
+    return render(request, 'bills/home.html', context)
+    #return HttpResponse('<h1> Billing Home </h1>')
+
+class BillListView(LoginRequiredMixin, ListView):
+    model = Bill
+    template_name = 'bills/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'bills'
+    ordering = ['-date_posted']
+
+class BillDetailView(LoginRequiredMixin, DetailView):
+    model = Bill
+    
+class BillCreateView(LoginRequiredMixin, CreateView):
+    model = Bill
+    fields = ['title', 'content', 'cost']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BillUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Bill
+    fields = ['title', 'content', 'cost']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        bill = self.get_object()
+        if self.request.user == bill.author:
+            return True
+        return False
+
+class BillDeleteView(LoginRequiredMixin, DeleteView):
+    model = Bill
+    success_url = '/'
+
+def about(request):
+    return render(request, 'about.html',{'title' : 'About'})
+    #return HttpResponse('<h1> Billing About</h1>')
+
+
+    
